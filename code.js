183b35508ef57e32a1d35b672f124ec258e40aa4
Abidijihed <abidij55@gmail.com>
2021-01-11 01:01:58 +0100

    im done

--- code.js
+++ code.js
@@ -1,8 +1,11 @@
+ 
+ var mony1=110;
+  var mony= $("#text");
 var imageArray = [
-            "img/AS.jpg",
        "img/7cor.png",
        "img/9.jpg",
        "img/8trfl.png",
+       "img/AS.jpg",
        "img/10_carreau.gif",
        "img/asCa.jpg",
        "img/J.jpg",
@@ -22,35 +25,58 @@ var imageArray = [
        "img/Qc.jpg",
        "img/qC.png",
        "img/kp.png"
-  
 ];
+var ammount1= document.getElementById("ammount1")
+var text=document.getElementById("text")
+var Withdraw=document.getElementById("Withdraw")
+Withdraw.addEventListener("click",function(){
+      mony.text(mony1-=(ammount1.value-""))
+
+})
 
 const image = document.querySelector("img");
 var button2 = document.querySelector("#button2");
 var button1 = document.querySelector("#button1");
-
-window.onload = () => generateRandomPicture(imageArray);
-
+ window.onload = () => generateRandomPicture(imageArray);
 button2.addEventListener("click", () => generateRandomPicture(imageArray));
+
 button1.addEventListener("click", () => generateRandomPicture(imageArray));
+ var randomNum=0
 function generateRandomPicture(array){
-	
+  
   var randomNum = Math.floor(Math.random() * array.length); 
    image.setAttribute("src", array[randomNum]);
- 
-  for (var i = 0; i <imageArray.length; i++) {
+ var i=0
+  while (mony1!==0 && i<array.length){
   
-  if(i===randomNum){
-    alert ("you guess")
-  }else if(i!==randomNum){
-     return ("try again you can guess")
-  
+  if(i===Math.floor(randomNum/2)){
+    mony1=mony1+40
+mony.text(mony1)
+    //text.innerHTML=mony1+40
+    alert ("you guess");
   }
-   
-}
+  else{
+    mony1=mony1-5
+    mony.text(mony1)
+    //text.innerHTML=mony1-10
+    return ("repeat again")
+  }
+i++
+  }
+   alert("your don't have enough money")
 }
 
-function validate(){
+var ammount = document.getElementById("ammount")
+
+var text=document.getElementById("text")
+var Diposit=document.getElementById("Diposit")
+Diposit.addEventListener("click",function(){
+  console.log('ammount',typeof ammount.value )
+      mony.text(mony1 += (ammount.value-"") )
+
+})
+
+function validate(){typeof
   console.log('here')
   var username=document.getElementById("username");
   var password=document.getElementById("password");
@@ -58,7 +84,14 @@ function validate(){
     // alert("login succesfully");
     location.href = "jocker.html"
   }else{
-    alert("login failed")
+    alert("username or password wrong")
     
       }
 }
+
+   
+
+   
+    
+    
+  
